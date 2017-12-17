9343555a90550be21e79c58d9abdd3764093dc28
Vignesh Murugesan <vigneshm@Vigneshs-MacBook-Pro.local>
2017-12-17 14:51:41 -0800

    basic menus and stock viz

--- crystal/crystal.py
+++ crystal/crystal.py
@@ -1,12 +1,18 @@
-from pandas_datareader import data
-import pandas as pd
-
 from datetime import (
     datetime,
     timedelta,
 )
+import sys
 
-_DATA_SOURCE = 'google'
+from utils.commands import (
+    Command,
+    Commander,
+)
+
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+
+_DATA_SOURCE = 'yahoo'
 _CATEGORY_CLOSE = 'Close'
 
 
@@ -16,34 +22,51 @@ def timestamp(date):
 
 
 def get_stock_data(
-        ticker_list,
+        symbol,
         start_timestamp,
         end_timestamp,
         category,
 ):
-    panel_date = data.DataReader(ticker_list, _DATA_SOURCE, start_timestamp, end_timestamp)
+    panel_date = data.DataReader([symbol], _DATA_SOURCE, start_timestamp, end_timestamp)
     close = panel_date.ix[category]
-    all_weekdays = pd.date_range(start=start_timestamp, end=end_timestamp, freq='B')
 
     # Align the existing prices in adj_close with our new set of dates by
     # reindexing close using all_weekdays as the new index
-    close = close.reindex(all_weekdays)
+    # all_weekdays = pd.date_range(start=start_timestamp, end=end_timestamp, freq='B')
+    # close = close.reindex(all_weekdays)
     return close
 
 
-def get_yearly_close_data(ticker_list):
-    return get_stock_data(
-        ticker_list,
+def plot_stock_data(symbol, start_ts, end_ts, category):
+    close = get_stock_data(symbol, start_ts, end_ts, category)
+    plt.plot(close)
+    plt.ylabel('time')
+    plt.xlabel('stock value')
+    plt.title('stock - ' + symbol + ', category: ' + category)
+    plt.draw()
+    plt.show()
+
+
+def get_yearly_close(symbol):
+    return plot_stock_data(
+        symbol,
         timestamp(datetime.today() - timedelta(days=365)),
         timestamp(datetime.today()),
         _CATEGORY_CLOSE,
     )
 
 
-def run():
-    close_list = get_yearly_close_data(['MSFT', 'AAPL'])
-    import ipdb;
-    ipdb.set_trace()
+def plot_data(list):
+    pass
 
 
-run()
+def commander():
+    cmd = Commander([
+        Command(title='Exit', action=lambda: sys.exit()),
+        Command(title='Get a stock quote for MSFT', action=lambda: get_yearly_close('MSFT')),
+        Command(title='Get a stock quote for AAPL', action=lambda: get_yearly_close('AAPL')),
+    ])
+    cmd.run()
+
+
+commander()
---
+++ crystal/utils/commands.py
@@ -0,0 +1,21 @@
+class Commander(object):
+    def __init__(self, commands):
+        self.commands = commands
+
+    def run(self):
+        while True:
+            print "\nPick one:"
+            for idx, command in enumerate(self.commands):
+                print str(idx + 1) + ": " + command.title
+            selection = int(input("Enter choice: "))
+            if 1 <= selection <= len(self.commands):
+                self.commands[selection-1].run()
+
+
+class Command(object):
+    def __init__(self, title, action):
+        self.title = title
+        self.action = action
+
+    def run(self):
+        self.action()
